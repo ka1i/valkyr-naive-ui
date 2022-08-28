f433f537a1bc91a06b37511eb31da275cef24ed0
ka1i <Mardan137@outlook.com>
2022-08-28 22:27:39 +0800

    project configure

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { resolve } from "path";
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
+  server: {
+    port: 2333,
+    host: "0.0.0.0",
+    strictPort: true,
+  },
+  preview: {
+    port: 2333,
+    host: "0.0.0.0",
+    strictPort: true,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "*": resolve(__dirname, ""),
+      "@": resolve(__dirname, "src"),
+    },
+  },
 })
