3f4b5167a0524b66a492cf03f11d7c2f002e83d2
ka1i <Mardan137@outlook.com>
2022-09-02 00:26:35 +0800

    test: listen global event

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,53 @@ import type { App } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router'
 import LayoutView from '/@/layouts/default/index.vue'
 
+import Request from '/@/views/Container/request/Request.vue'
+import Message from '/@/views/Container/message/Message.vue'
+import Dialog from '/@/views/Container/dialog/Dialog.vue'
+import Notification from '/@/views/Container/notification/Notification.vue'
+import LoadingBar from '/@/views/Container/loadingBar/LoadingBar.vue'
+
 const routes = [
   {
     path: '/',
     name: 'Layout',
     component: LayoutView,
     children: [
-      { path: "/", name: "Home", component: () => import('/@/views/Home.vue'), },
-      { path: "/login", name: "Login", component: () => import('/@/views/Login/index.vue'), }
+      {
+        path: "/",
+        name: "Home",
+        component: () => import('/@/views/Home.vue'),
+      },
+      {
+        path: "/login",
+        name: "Login",
+        component: () => import('/@/views/Login/index.vue'),
+      },
+      {
+        path: "/request",
+        name: "Request",
+        component: Request,
+      },
+      {
+        path: "/message",
+        name: "Message",
+        component: Message,
+      },
+      {
+        path: "/dialog",
+        name: "Dialog",
+        component: Dialog,
+      },
+      {
+        path: "/notification",
+        name: "Notification",
+        component: Notification,
+      },
+      {
+        path: "/loadingBar",
+        name: "LoadingBar",
+        component: LoadingBar,
+      }
     ]
   }
 ];
---
+++ src/views/Container/loadingBar/LoadingBar.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<n-h3>LoadingBar</n-h3>
+		<n-button type="primary" @click="handleShowLoadingBar">
+			useLoadingBar
+		</n-button>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useLoadingBar } from 'naive-ui'
+import type { LoadingBarProviderInst, } from 'naive-ui'
+
+export default defineComponent({
+	name: 'Message',
+	setup() {
+		const loadingBar = useLoadingBar() as LoadingBarProviderInst
+		const handleShowLoadingBar = () => {
+			loadingBar.start()
+		}
+		return {
+			handleShowLoadingBar,
+		}
+	},
+})
+</script>
